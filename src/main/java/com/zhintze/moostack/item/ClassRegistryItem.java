15c144d64d9de739e03dcd69d2b768cdbe5377ca
zhintze <zachhintze@gmail.com>
2026-01-31 15:00:41 -0700

    Add ClassRegistryItem (pending screen implementation)
    - Item opens class selection GUI when right-clicked
    - Checks if player already has selected role and shows warning
    - Uses StarterRoleData attachment for role persistence
    - Has epic rarity, single stack, enchantment glint
    - Note: Will not compile until ClassRegistryScreen is created

---
+++ src/main/java/com/zhintze/moostack/item/ClassRegistryItem.java
@@ -0,0 +1,59 @@
+package com.zhintze.moostack.item;
+
+import com.zhintze.moostack.client.screen.ClassRegistryScreen;
+import com.zhintze.moostack.starterrole.StarterRoleAttachments;
+import com.zhintze.moostack.starterrole.StarterRoleData;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
+
+public class ClassRegistryItem extends Item {
+
+    public ClassRegistryItem(Properties properties) {
+        super(properties.stacksTo(1).rarity(Rarity.EPIC));
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        ItemStack stack = player.getItemInHand(hand);
+
+        if (level.isClientSide()) {
+            StarterRoleData data = player.getData(StarterRoleAttachments.STARTER_ROLE);
+            if (data.hasSelectedRole()) {
+                player.displayClientMessage(
+                    Component.translatable("moostack.class_registry.already_selected",
+                        data.getSelectedRole().getDisplayName())
+                        .withStyle(ChatFormatting.YELLOW),
+                    true
+                );
+            } else {
+                ClassRegistryScreen.open(hand);
+            }
+            return InteractionResultHolder.success(stack);
+        }
+
+        return InteractionResultHolder.consume(stack);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltip, TooltipFlag flag) {
+        tooltip.add(Component.translatable("moostack.class_registry.tooltip")
+            .withStyle(ChatFormatting.GRAY));
+        tooltip.add(Component.translatable("moostack.class_registry.tooltip.instruction")
+            .withStyle(ChatFormatting.DARK_GRAY, ChatFormatting.ITALIC));
+    }
+
+    @Override
+    public boolean isFoil(ItemStack stack) {
+        return true;
+    }
+}
